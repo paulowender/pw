71c7d98e048f2bfe15bbe035ddecd42e80b52587
Paulo Wender de Almeida Oliveira <paulowender2011@gmail.com>
2022-02-11 21:35:11 -0400

    Clean theme code

--- lib/pwtheme.dart
+++ lib/pwtheme.dart
@@ -1,55 +1,43 @@
 import 'package:flutter/material.dart';
 
 class PWTheme {
-  static const Color white = Color(0XFFFFFFFF);
-  static const Color black = Color(0XFF000000);
-  static const Color cardLight = Color(0XFFFFFFFF);
-  static const Color redLight = Color(0XFFEF9A9A);
-  static const Color redDark = Color(0XFFB72C2C);
-  static const Color green = Color(0XFF43A047);
-  static const Color greenAcenpt = Color(0XFFA5D6A7);
-  static const Color errorLight = Color(0XFFFFCDD2);
-  static const Color errorDark = Color(0XFFB71C1C);
-  static const Color blueLight = Color(0XFFBBDEFB);
-  static const Color blueDark = Color(0XFF1E88E5);
-
   static ThemeData light() {
     ThemeData theme = ThemeData(
-        primaryColor: green,
+        primaryColor: const Color(0XFF43A047),
         dividerColor: Colors.green[400],
         scaffoldBackgroundColor: Colors.blueGrey[50],
         iconTheme: IconThemeData(color: Colors.green[600]),
         colorScheme: const ColorScheme.light(
-          primary: green,
-          secondary: greenAcenpt,
-          secondaryContainer: blueLight,
+          primary: Color(0XFF43A047),
+          secondary: Color(0XFFA5D6A7),
+          secondaryContainer: Color(0XFFBBDEFB),
           onPrimary: Colors.white,
           onSecondary: Color(0XFF43A047),
-          error: redDark,
+          error: Color(0XFFB72C2C),
           primaryContainer: Colors.white,
         ),
         cardTheme: const CardTheme(
-          color: cardLight,
+          color: Color(0XFFFFFFFF),
         ));
     return theme;
   }
 
   static ThemeData dark() {
     ThemeData theme = ThemeData(
-        primaryColor: white,
-        dividerColor: green,
+        primaryColor: const Color(0XFFFFFFFF),
+        dividerColor: const Color(0XFF43A047),
         iconTheme: const IconThemeData(color: Colors.white),
         colorScheme: const ColorScheme.dark(
-          primary: green,
+          primary: Color(0XFF43A047),
           secondary: Colors.green,
-          secondaryContainer: blueDark,
+          secondaryContainer: Color(0XFF1E88E5),
           onPrimary: Colors.white,
           onSecondary: Colors.white,
-          error: redDark,
-          inversePrimary: black,
+          error: Color(0XFFB72C2C),
+          inversePrimary: Color(0XFF000000),
           // primaryContainer: Color.fromARGB(255, 43, 42, 42),
         ),
-        appBarTheme: const AppBarTheme(backgroundColor: green));
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0XFF43A047)));
     return theme;
   }
 }
